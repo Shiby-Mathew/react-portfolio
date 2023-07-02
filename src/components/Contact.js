b6057187a531ae047e7a360456de166f2df58ec6
Shiby-Mathew <shiby.ajay@gmail.com>
2023-07-02 12:31:25 +1000

    Added contact form, still working on it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,70 @@
 import React from "react";
+import "./styles/contact.css";
 
 function Contact() {
+  const handleInputChange = (e) => {
+    // const { name, value } = e.target;
+    // return name === 'firstName' ? setFirstName(value) : setLastName(value);
+  };
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // // Alert the user their first and last name, clear the inputs
+    // alert(`Hello ${firstName} ${lastName}`);
+    // setFirstName('');
+    // setLastName('');
+  };
+
   return (
     <div>
-      <p className="content is-medium">Full Stack Developer</p>
+      <p className="contact">Contact Me</p>
       <hr />
+      <form className="contactForm">
+        <div className="form">
+          <div className="form-group mb-4">
+            <label className="label p-2">Name</label>
+            <input
+              className="input "
+              // value={name}
+              name="name"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Name"
+            />
+          </div>
+          <div className="form-group mb-4">
+            <label className="label p-2">Email Address</label>
+            <input
+              className="input"
+              // value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group">
+            <label className="label p-2 ">Message</label>
+            <textarea
+              className="textarea"
+              // value={message}
+              name="message"
+              onChange={handleInputChange}
+              rows="5"
+              placeholder="Message"
+            />
+          </div>
+        </div>
 
-      <p className="content is-italic mt-4">
-        Studying the full stack to better my work
-      </p>
-      <p className="content">
-        This is Contact Page PageI always act as a product manager for learning
-        resources, which guides users to what they need in the moment, but also
-        enable them to explore content on their terms using video, audio,
-        documentation, and so much more in an advanced learning ecosystem.
-      </p>
+        <button
+          type="submit"
+          class="btn btn-primary mt-4"
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
